00277258c5a9ba36b71b347478b96a8f543257c8
Alexandru Iacoboae <alexandru.iacoboae@certent.com>
2020-04-30 15:34:53 +0300

    Adding the code for a simple StopWatch Console App to GitHub Repo

--- Program.cs
+++ Program.cs
@@ -1,15 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System;
+
 
 namespace StopWatchConsoleAPP
 {
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            string command;
+            var myStopwatch = new StopWatch();
+
+            try
+            {
+                Console.WriteLine("Hello my brother from another mother! Please let me now your action by typping the action name\n" +
+                "Start\n" +
+                "Stop\n" +
+                "Exit\n");
+
+                command = Console.ReadLine();
+
+
+                while (!string.IsNullOrEmpty(command) && (command != "exit"))
+                {
+
+                    switch (command.ToLower())
+                    {
+                        case "start":
+                            myStopwatch.Start();
+                            Console.WriteLine("Please let me know when to stop by typing: Stop");
+                            command = Console.ReadLine();
+                            break;
+                        case "stop":
+                            myStopwatch.Stop();
+                            Console.WriteLine("The total duration measured is: {0}\n", myStopwatch.Duration.ToString(@"hh\:mm\:ss"));
+                            Console.WriteLine("To start again the StopWatch type: Start\n" +
+                                "If you want to exit this awsome aplication please type: exit\n"
+                                );
+                            command = Console.ReadLine();
+                            break;
+
+                        default:
+                            Console.WriteLine("Please type a valid command");
+                            command = Console.ReadLine();
+                            break;
+                    }
+                }
+
+            }
+
+            catch (Exception)
+            {
+
+                Console.WriteLine("Invalid Operation");
+            }
+
+            
+
+                        
+                
+
+
+            
+
+
         }
     }
 }
---
+++ StopWatch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StopWatchConsoleAPP
+{
+    public class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _stopTime;
+        private bool _isStarted = false;
+
+        public TimeSpan Duration
+        {
+            get { return _startTime - _stopTime; }
+        }
+
+        public void Start()
+        {
+            if (!_isStarted)
+            {
+                _startTime = DateTime.Now;
+                _isStarted = true;
+            }
+        }
+
+        public void Stop()
+        {
+            _stopTime = DateTime.Now;
+            _isStarted = false;
+        }
+
+    }
+}
